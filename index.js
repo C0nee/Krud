7a4b95c9e20fbcefe1a8ef9d3918c37c37e0b0d9
C0nee <jakubczajkowski04@wp.pl>
2023-11-15 14:22:45 +0100

    sdawesaf

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-
+app.use(bodyParser.urlencoded({extended:true}));
 const db = require('./db');
 
 
@@ -12,6 +12,7 @@ app.get('/', async (req, res) => {
 
 
 app.get('/listAll', async (req, res) => {
+    res.setHeader('content-type','text/html');
     res.write("<h1>Lista wszystkich rekordów w bazie</h1>");
     const client = await db.connect();
     res.write("<table>");
@@ -26,6 +27,41 @@ app.get('/listAll', async (req, res) => {
     db.close(client);
     res.end();
 })
+app.post('/search',async(req,res)=>{
+let criteria = req.body;
+//criteria.name = "/" +criteria.name+"/"
+const client = await db.connect();
+let list = await db.get(client,criteria);
+res.setHeader('content-type','text/html');
+res.write("<h1>Lista  rekordów w bazie spełniających kryteria</h1>");
+res.write("<table>");
+list.forEach(element => {
+    res.write("<tr>");
+    res.write("<td>" + element.listing_url + "</td>");
+    res.write("<td>" + element.name + "</td>");
+    res.write("</tr>");
+});
+res.write("</table>");
+db.close(client);
+res.end();
 
+});
+app.post('/add',async(req,res)=>{
+    let data = req.body;
+    const client = await db.connect();
+    const dbResponse = await db.add(client, data);
+    if(await db.add(client,data)){
+        res.setHeader('content-type','text/html');
+res.write("<h1>dodano rekord poprawnie</h1>");
+
+    }
+    else{
+        res.setHeader('content-type','text/html');
+        res.write("<h1>nastąpił błąd podczas dodawania rekordu</h1>");
+        
+    }
+    db.close(client);
+    res.end();
+})
 //yds
 app.listen(8000);
